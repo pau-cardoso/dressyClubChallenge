70ee86080758c6f9a6b0d669b2e36a18029077cf
pau-cardoso <paulinacardoso7@gmail.com>
2024-02-04 23:31:18 -0600

    feat: add navigation

--- cars-frontend/App.js
+++ cars-frontend/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import CarList from './screens/CarList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddCarScreen from './screens/AddCarScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <CarList />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={CarList} />
+        <Stack.Screen name="AddCar" component={AddCarScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
