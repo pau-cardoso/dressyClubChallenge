4aa3cc806821639a8e18fd390cfe54e7e440e737
pau-cardoso <paulinacardoso7@gmail.com>
2024-02-04 22:46:28 -0600

    feat: add update method in cars controller

--- cars-backend/src/cars/cars.controller.ts
+++ cars-backend/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
 import { CarsService } from './cars.service';
 import { Car } from './schemas/car.schema';
@@ -15,5 +15,13 @@ export class CarsController {
   @Get()
   async findAll(): Promise<Car[]> {
     return this.carService.findAll();
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') carId: string,
+    @Body() updateCarDto: CreateCarDto,
+  ): Promise<Car> {
+    return this.carService.updateCar(carId, updateCarDto);
   }
 }
--- cars-backend/src/cars/cars.service.ts
+++ cars-backend/src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './schemas/car.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -22,5 +22,17 @@ export class CarsService {
       .populate('brand', 'name')
       .populate('color', 'name')
       .exec();
+  }
+
+  async updateCar(carId: string, updateCarDto: CreateCarDto): Promise<Car> {
+    const updatedCar = await this.carModel.findByIdAndUpdate(
+      carId,
+      updateCarDto,
+      { new: true },
+    );
+    if (!updatedCar) {
+      throw new NotFoundException(`Car with ID ${carId} not found`);
+    }
+    return updatedCar;
   }
 }
