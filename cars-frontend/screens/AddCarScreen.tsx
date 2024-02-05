cc3cdc7641cd974381e1b8e457d5336aadc0f01f
pau-cardoso <paulinacardoso7@gmail.com>
2024-02-05 02:52:14 -0600

    fix: bottom extra space in AddCarScreen

--- cars-frontend/screens/AddCarScreen.tsx
+++ cars-frontend/screens/AddCarScreen.tsx
@@ -83,51 +83,51 @@ export default function AddCarScreen({style, navigation, route}) {
   }
 
   return (
-    <ScrollView style={{width: '100%'}} contentContainerStyle={styles.container}>
-      <KeyboardAvoidingView style={styles.containerForm} behavior='height' enabled>
-        {/* <View style={styles.containerForm}> */}
-          <Text style={styles.title}>Add Car</Text>
-          <TextField
-            label='Model'
-            value={model}
-            onChangeText={setModel}
-          />
-          <PickerSelect
-            label='Brand'
-            items={brandOptions}
-            onValueChange={(value) => setSelectedBrand(value)}
-            value={selectedBrand}
-          />
-          <PickerSelect
-            label='Main Color'
-            items={colorOptions}
-            onValueChange={(value) => setSelectedColor(value)}
-            value={selectedColor}
-          />
-          <TextField
-            label='Value'
-            value={value}
-            onChangeText={setValue}
-            inputMode='numeric'
-          />
-          <TextField
-            label='Production Cost'
-            value={productionCost}
-            onChangeText={setProductionCost}
-            inputMode='numeric'
-          />
-          <TextField
-            label='Transportation Cost'
-            value={transportationCost}
-            onChangeText={setTransportationCost}
-            inputMode='numeric'
-          />
+    <KeyboardAvoidingView style={styles.container} behavior='height' enabled>
+      <ScrollView contentContainerStyle={styles.containerForm} style={{width: '100%', height: '100%', flexGrow: 1, flex: 1}}>
+        <Text style={styles.title}>Add Car</Text>
+        <TextField
+          label='Model'
+          value={model}
+          onChangeText={setModel}
+        />
+        <PickerSelect
+          label='Brand'
+          items={brandOptions}
+          onValueChange={(value) => setSelectedBrand(value)}
+          value={selectedBrand}
+        />
+        <PickerSelect
+          label='Main Color'
+          items={colorOptions}
+          onValueChange={(value) => setSelectedColor(value)}
+          value={selectedColor}
+        />
+        <TextField
+          label='Value'
+          value={value}
+          onChangeText={setValue}
+          inputMode='numeric'
+        />
+        <TextField
+          label='Production Cost'
+          value={productionCost}
+          onChangeText={setProductionCost}
+          inputMode='numeric'
+        />
+        <TextField
+          label='Transportation Cost'
+          value={transportationCost}
+          onChangeText={setTransportationCost}
+          inputMode='numeric'
+        />
+        <View style={{marginBottom: 150}}>
           <AppButton onPress={addCar}>
             Save
           </AppButton>
-        {/* </View> */}
-      </KeyboardAvoidingView>
-    </ScrollView>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -139,25 +139,17 @@ const styles = StyleSheet.create({
     color: '#213B64',
   },
   container: {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
     paddingHorizontal: 16,
-    paddingBottom: 200,
+    flex: 1,
   },
   containerForm: {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 24,
     maxWidth: 600,
     marginTop: 48,
   },
-  item: {
-    marginBottom: 12,
-  }
 });
