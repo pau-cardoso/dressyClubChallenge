ac1269c4aee4ce57a19ed36b7ff585638751a881
pau-cardoso <paulinacardoso7@gmail.com>
2024-02-04 23:06:56 -0600

    add: custom hooks for fetching data

---
+++ cars-frontend/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return [data];
+}
